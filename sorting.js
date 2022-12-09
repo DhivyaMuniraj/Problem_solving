0309604a53788901a445b06fb1ce9eaeb2b47e78
Dhivya Muniraj <dhiviya.muniraj@cesltd.com>
2022-12-09 21:36:04 +0530

    Sorting

---
+++ sorting.js
@@ -0,0 +1,11 @@
+/*
+* Get an input
+* Create a function with array as parameter
+* create a temporary variable
+* run the first  i loop from 0 to till array length
+* run the second j loop from i to till array length
+* check the condition if first element is greater than second
+* if yes just swap and check with all element
+* after the second loop got over then increment the first loop
+* return the array
+*/
